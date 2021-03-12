647f4d2e8c83784af90bb31c8a6eeb97241f5730
Ekaterina <78584614+AvoCadoRina@users.noreply.github.com>
2021-03-12 21:43:47 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 
- function maximum(n){
-    var max = n[0];
+ function max(n){
+    var maxi = n[0];
     for (var i = 0; i < n.length; i++) {
-        if (max < n[i]) max = n[i]; 
+        if (maxi < n[i]) maxi = n[i]; 
     }
     return max;
  }
 
- function minimum(n){
-    var min = n[0];
+ function min(n){
+    var mini = n[0];
     for (var i = 0; i > n.length; i++) {
-        if (min > n[i]) min = n[i]; 
+        if (mini > n[i]) mini = n[i]; 
     }
     return min;
  }
 
-function average(n) {
+function avg(n) {
    var sum = 0;
    for(var i = 0; i < n.length; i++) {
        sum += n[i];
