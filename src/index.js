872b1fb76b16e6bff09411c269272718f215afee
Ekaterina <78584614+AvoCadoRina@users.noreply.github.com>
2021-03-12 21:06:58 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,23 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+      return 0;
 }
+ 
+function avg(n)
+ {
+var arr = []
+let Summa = 0;
+let Items_all = 0;
+const len = arr.length;
+let item = null;
+for (let i = 0; i < len; i++) {
+    item = arr[i];
+    if (item.found) {
+        Summa = item.avg + Summa;
+        Items_all = Items_all + 1;
+    
+}
+const average_all = Summa / Items_all;
+console.log("Average is:", average_all);
+    
